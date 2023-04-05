4581ca3484543379f8148160281986f9159f6c8f
CarlosIgreda <cjigredarivas@gmail.com>
2023-04-05 03:00:56 -0500

    Add src files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
-import _ from 'lodash';
 import './styles.css';
-import printMe from './print.js';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+const tasks = [
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+];
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const renderTasks = (tasks) => {
+  const $taskList = document.querySelector('.ul-items');
+  $taskList.innerHTML = '';
+  tasks.sort((a, b) => a.index - b.index);
+  tasks.forEach((task) => {
+    const $taskListItem = document.createElement('li');
+    $taskListItem.setAttribute('class', 'li-item');
+    $taskListItem.innerHTML = `
+      <input type="checkbox" ${task.completed ? 'checked' : ''}>
+      <span class="span-text" contenteditable="true">${task.description}</span>
+    `;
+    const $spanButton = document.createElement('span');
+    $spanButton.innerText = '፧';
+    $spanButton.setAttribute('class', 'bt drag');
+    $taskListItem.insertAdjacentElement('afterend', $spanButton);
+    const $divItem = document.createElement('div');
+    $divItem.setAttribute('class', 'div-item');
+    $divItem.appendChild($taskListItem);
+    $divItem.appendChild($spanButton);
 
-  btn.innerHTML = 'Click me and check the console now!';
-  btn.onclick = printMe;
+    const checkbox = $taskListItem.querySelector('input[type = "checkbox"]');
+    checkbox.addEventListener('change', () => {
+      task.completed = checkbox.checked;
+    });
+    $taskList.appendChild($divItem);
+  });
+};
 
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
+document.addEventListener('DOMContentLoaded', () => {
+  renderTasks(tasks);
+});
