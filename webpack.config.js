2db980583e74acbcbb37bcfb3d12ea31f7d87d0b
CarlosIgreda <cjigredarivas@gmail.com>
2023-04-06 17:42:18 -0500

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     functionality: './src/functionality.js',
