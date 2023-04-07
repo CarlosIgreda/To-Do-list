80bce83931a2560a83e165d2bf46775f6f6c9f67
CarlosIgreda <cjigredarivas@gmail.com>
2023-04-07 16:50:02 -0500

    Update dist

---
+++ dist/save.bundle.js
@@ -0,0 +1,29 @@
+"use strict";
+(self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || []).push([["save"],{
+
+/***/ "./src/save.js":
+/*!*********************!*\
+  !*** ./src/save.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// Define a function to save the tasks in local storage
+const saveTasks = (tasks) => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (saveTasks);
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/save.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2F2ZS5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBOztBQUVBLGlFQUFlLFNBQVMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90by1kby1saXN0Ly4vc3JjL3NhdmUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gRGVmaW5lIGEgZnVuY3Rpb24gdG8gc2F2ZSB0aGUgdGFza3MgaW4gbG9jYWwgc3RvcmFnZVxuY29uc3Qgc2F2ZVRhc2tzID0gKHRhc2tzKSA9PiB7XG4gIGxvY2FsU3RvcmFnZS5zZXRJdGVtKCd0YXNrcycsIEpTT04uc3RyaW5naWZ5KHRhc2tzKSk7XG59O1xuXG5leHBvcnQgZGVmYXVsdCBzYXZlVGFza3M7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
